c33e7d5c5f5c12c866f12b4cd78b619fbf3d671b
jackjk521 <jedchu541@gmail.com>
2022-09-24 23:23:08 +0800

    final edits to the index.js and layout.js. Added css files

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,9 +1,7 @@
 
 import Image from 'next/image'
 import Head from 'next/head'
-import Script from 'next/script'
 import Link from 'next/link'
-import Layout from '../../components/layout'
 
 
 const CatImage = () => (
@@ -18,8 +16,7 @@ const CatImage = () => (
 
 export default function FirstPost() {
     return (
-   
-        <Layout>
+        <div>
             <Head>
                 <title>First Post</title>
 
@@ -38,8 +35,7 @@ export default function FirstPost() {
 
             <h1>Here is cat</h1>
             <CatImage />
-
-        </Layout>
-                
+        </div>
+            
     ); 
 }
