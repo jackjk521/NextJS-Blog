a489367491836ea75e7ba8e0dffff384b81b50e3
jackjk521 <jedchu541@gmail.com>
2022-09-27 08:36:58 +0800

    .vs folder doesn't exist and added a change in first post

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -6,7 +6,7 @@ export default function FirstPost() {
                 
             <h1>First Post</h1>
 
-            <h3>So excited!! *Kevin Hart's voice* </h1>
+            <h3>So excited!! *Kevin Hart's voice* </h3>
         </div> 
     ); 
 }
