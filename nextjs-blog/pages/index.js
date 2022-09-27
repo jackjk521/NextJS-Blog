c9793604bb5352e9774b90f9e3f3af9baf9e1c65
jackjk521 <jedchu541@gmail.com>
2022-09-27 21:58:20 +0800

    added md files, edited index.js to show md files, added caustic image

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -3,8 +3,17 @@ import Link from 'next/link';
 import Script from 'next/script'
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData();
+    return {
+        props: {
+            allPostsData,
+        },
+    };
+}
+export default function Home({allPostsData}) {
  
     return (
        
@@ -21,23 +30,40 @@ export default function Home() {
                 /> // won't be added
       </Head>
 
-        <Script
-            src="https://cat-bounce.com/"
-            strategy="lazyOnload"
-            onLoad={() =>
-                console.log(`script loaded correctly, cats are now falling `)
-            }
+            <Script
+                src="https://cat-bounce.com/"
+                strategy="lazyOnload"
+                onLoad={() =>
+                    console.log(`script loaded correctly, cats are now falling `)
+                }
             />
+
+
             <Layout home> //home is the prop
                 <Head>
                     <title>{siteTitle}</title>
                 </Head>
 
                 <section className={utilStyles.headingMd}>
-                    <p>[Your Self Introduction]</p>
+                    <p> Trader - Blockchain Enthusiast - Investor - Organizer - Challenger </p>
                     <h1 className={utilStyles.headingMd}>
                         Read <Link href="/posts/first-post"> this my first-post here!</Link>
                     </h1>
+                </section>
+
+                <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                    <h2 className={utilStyles.headingLg}>Blog</h2>
+                    <ul className={utilStyles.list}>
+                        {allPostsData.map(({ id, date, title }) => (
+                            <li className={utilStyles.listItem} key={id}>
+                                {title}
+                                <br />
+                                {id}
+                                <br />
+                                {date}
+                            </li>
+                        ))}
+                    </ul>
                 </section>
             </Layout>
      
