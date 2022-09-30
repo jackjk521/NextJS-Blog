feace4db734d97b33e005f42d9fcff4041ba2414
jackjk521 <jedchu541@gmail.com>
2022-09-30 21:51:05 +0800

    edited posts.js and added [id].js under the pages/posts folder for dynamic routing and added a date.js component

--- nextjs-blog/lib/posts.js
+++ nextjs-blog/lib/posts.js
@@ -2,6 +2,10 @@ import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 
+// for converting markdown files to HTML strings
+import { remark } from 'remark';
+import html from 'remark-html';
+
 const postsDirectory = path.join(process.cwd(), 'posts'); // root/posts (folder)
 
 export async function peopleApi() {
@@ -24,6 +28,39 @@ export async function peopleApi() {
     return data;
 
   
+}
+
+export function getAllPostIds() { // returns a list of filenames as ids 
+    const fileNames = fs.readdirSync(postsDirectory);
+
+    return fileNames.map((fileName) => {
+        return {
+            params: { // returns an array  
+                id: fileName.replace(/\.md$/, ''),
+            },
+        };
+    });
+}
+
+export async function getPostData(id) {
+    const fullPath = path.join(postsDirectory, `${id}.md`);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+
+    // Use remark to convert markdown into HTML string
+    const processedContent = await remark()
+        .use(html)
+        .process(matterResult.content);
+    const contentHtml = processedContent.toString();
+
+    // Combine the data with the id and contentHtml
+    return {
+        id,
+        contentHtml,
+        ...matterResult.data,
+    };
 }
 
 export function getSortedPostsData() {
