8f099feddc5fc3867f69b6b2a43439a78a5692fe
jackjk521 <jedchu541@gmail.com>
2022-10-03 14:35:58 +0800

    added api.js and work item
    Related Work Items: #124745

---
+++ nextjs-blog/pages/api.js
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+
+    const email = req.body.email; // sample request to retrieve the value of an input
+
+    res.status(200).json({ text: 'Hello' });
+}
