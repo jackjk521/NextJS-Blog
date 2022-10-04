eb617db8b7eb0090ec525a5cce17d5454f5c634a
jackjk521 <jedchu541@gmail.com>
2022-10-04 19:27:18 +0800

    added world in the test

--- nextjs-blog/pages/api.js
+++ nextjs-blog/pages/api.js
@@ -2,5 +2,5 @@ export default function handler(req, res) {
 
     const email = req.body.email; // sample request to retrieve the value of an input
 
-    res.status(200).json({ text: 'Hello' });
+    res.status(200).json({ text: 'Hello World' });
 }
